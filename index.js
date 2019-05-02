49e152fd1f39d6ea6db30b0a733d9dbfbbd5de71
orawan2529 <orawan2529@gmail.com>
2019-05-02 10:24:25 +0700

    Add generate jwt.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
   next();
 });
 
